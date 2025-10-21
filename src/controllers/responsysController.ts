4b3970194b3be63ca82aa98a2cb9fa0788b4e578
duonghuu <huuduong9@gmail.com>
2025-10-21 18:43:45 +0700

    feat: abc

--- src/controllers/responsysController.ts
+++ src/controllers/responsysController.ts
@@ -1,31 +1,110 @@
 import { redis } from "@config/redis";
+import { ResponsysService } from "@services/responsysService";
 import { jobQueue } from "@workers/queue";
 import dayjs from "dayjs";
 import { Request, Response } from "express";
 import fs from "fs";
+import _ from "lodash";
+import { v4 as uuidv4 } from 'uuid';
+
+function formatName(str: string) {
+
+    str = str.replace(/  +/g, ' ');
+
+    let arrTemp = str.split(' ');
+
+    const objName: { LAST_NAME?: string; MIDDLE_NAME?: string; FIRST_NAME?: string } = {};
+    if (arrTemp.length > 0) {
+
+        objName['LAST_NAME'] = arrTemp[0];
+
+        if (arrTemp.length > 1) {
+            objName['FIRST_NAME'] = arrTemp[arrTemp.length - 1];
+        }
+
+        if (arrTemp.length > 2) {
+            arrTemp.splice(0, 1);
+            arrTemp.splice(arrTemp.length - 1, 1);
+            objName['MIDDLE_NAME'] = arrTemp.join(' ');
+        }
+    }
+
+
+    return objName;
+}
 
 export const responsysController = {
     async handleRegister(req: Request, res: Response): Promise<void> {
-        // res.send(req.body);
-        const endpoint = `/rest/api/v1.3/folders/Banking/suppData/Lead_Form_Data/members`
-        // const jobId = `${endpoint}_${Date.now()}`; // identify theo endpoint
-        // const data = {
-        //     endpoint
-        // }
-        const job = await jobQueue.add('sendRequest', { endpoint, name: req.body.name, email: req.body.email });
-        fs.appendFileSync(
-            "logs/receive.log",
-            `[${dayjs().format("D/M/YYYY H:mm:ss")}] ${job?.id}\n`
-        );
-        // res.json({ success: true });
+        let json = req.body ? req.body.data : {};
 
-        // Thêm phần tử vào list
-        // await redis.rpush("queue", "job1");
-        // await redis.rpush("queue", "job2");
-        // const job = await redis.lpop("queue");
-        // res.json({ job });
+        //check data
+        if (!json.CUSTOMER_ID_ && !json.EMAIL_ADDRESS_ && !json.MOBILE_NUMBER_) {
+            res.send({ ok: false, message: 'Missing customer data' });
+            return;
+        }
 
-        // await jobQueue.add("sendEmail", { to: `user${new Date().toISOString()}@example.com` });
-        res.json({ success: true });
+        let data = req.body;
+        data.data = _.pick(data.data, ['CUSTOMER_ID_', 'EMAIL_ADDRESS_', 'MOBILE_NUMBER_', 'FIRST_NAME', 'MIDDLE_NAME', 'LAST_NAME', 'LEAD_SOURCE', 'MKT_CAMPAIGN', 'MKT_FORM', 'ZALO_ID']);
+
+        if (json.FIRST_NAME && !json.LAST_NAME) {
+            let format = formatName(json.FIRST_NAME);
+
+            data.data.FIRST_NAME = format.FIRST_NAME ? format.FIRST_NAME : data.data.FIRST_NAME;
+            data.data.LAST_NAME = format.LAST_NAME ? format.LAST_NAME : data.data.LAST_NAME;
+            data.data.MIDDLE_NAME = format.MIDDLE_NAME ? format.MIDDLE_NAME : data.data.MIDDLE_NAME;
+        }
+
+        else if (!json.FIRST_NAME && json.LAST_NAME) {
+            let format = formatName(json.LAST_NAME);
+
+            data.data.FIRST_NAME = format.FIRST_NAME ? format.FIRST_NAME : data.data.FIRST_NAME;
+            data.data.LAST_NAME = format.LAST_NAME ? format.LAST_NAME : data.data.LAST_NAME;
+            data.data.MIDDLE_NAME = format.MIDDLE_NAME ? format.MIDDLE_NAME : data.data.MIDDLE_NAME;
+        }
+
+        //override
+        if (json.CUSTOMER_ID_) {
+            data.matchColumnName1 = 'CUSTOMER_ID_';
+            data.matchColumnName2 = null;
+        }
+
+        else if (json.MOBILE_NUMBER_) {
+            data.matchColumnName1 = 'MOBILE_NUMBER_';
+            data.matchColumnName2 = null;
+        }
+
+        else if (json.EMAIL_ADDRESS_) {
+            data.matchColumnName1 = 'EMAIL_ADDRESS_';
+            data.matchColumnName2 = null;
+        }
+
+        let result = await ResponsysService.register(data);
+
+        responsysQueue.add({
+            type: 'SYNC_LEAD',
+            data: {
+                CUSTOMERID: json.CUSTOMER_ID_,
+                EMAILADDRESS: json.EMAIL_ADDRESS_,
+                MOBILENUMBER: json.MOBILE_NUMBER_,
+                FULL_NAME: json.FIRST_NAME ? json.FIRST_NAME + " " + json.LAST_NAME : json.LAST_NAME,
+                LEAD_SOURCE: json.LEAD_SOURCE,
+                MKT_CAMPAIGN: json.MKT_CAMPAIGN,
+                MKT_FORM: json.MKT_FORM,
+                UTM: json.UTM,
+                DATE_OF_BIRTH: json.DATE_OF_BIRTH,
+                CURRENT_ADDRESS: json.CURRENT_ADDRESS,
+                PROVINCE: json.PROVINCE,
+                RECORDID: json.CRMID ? json.CRMID : uuidv4(),
+                LEADNEXTVALID: json.LEADNEXTVALID,
+                PRODUCT: json.PRODUCT,
+                NOTIFIED_ID: json.NOTIFIED_ID,
+                SALE_CHANNEL: json.SALE_CHANNEL,
+                WEB_LEADID: json.WEB_LEADID,
+                GCLID: json.GCLID,
+                CX_SOURCE: "WEBSITE"
+            }
+        });
+
+        res.send({ data: result });
     },
 };
---
+++ src/services/responsysService.ts
@@ -0,0 +1,93 @@
+import axios from "axios";
+
+export const ResponsysService = {
+    authToken: null,
+    endPoint: "https://example.responsys.com", // Replace with actual endpoint
+    API_PROFILE_LIST: 'Resp_Banking_Customers',
+    async processContact(pk1: string, pk2: string, data: any): Promise<any> {
+        // if (!authToken) {
+        //     await authenticate();
+        // }
+
+        let result = await this.callContactAPI(pk1, pk2, data);
+        return result;
+    },
+
+    async callContactAPI(pk1: string, pk2: string, data: any): Promise<any> {
+        //call responsys api
+
+        let result = null;
+
+        let fieldNames = [];
+        let record = [];
+
+        for (let key in data) {
+            fieldNames.push(key);
+            record.push(data[key]);
+        }
+
+        let recordData = {
+            "recordData": {
+                "fieldNames": fieldNames,
+                "records": [
+                    record
+                ],
+                "mapTemplateName": null
+            },
+            "mergeRule": {
+                "htmlValue": "H",
+                "optinValue": "I",
+                "textValue": "T",
+                "insertOnNoMatch": true,
+                "updateOnMatch": "REPLACE_ALL",
+                "matchColumnName1": pk1,
+                "matchColumnName2": pk2,
+                "matchOperator": "NONE",
+                "optoutValue": "O",
+                "rejectRecordIfChannelEmpty": null,
+                "defaultPermissionStatus": "OPTIN"
+            }
+        }
+
+        try {
+            const config = {
+                method: 'post',
+                url: `${this.endPoint}/rest/api/v1.3/lists/${this.API_PROFILE_LIST}/members`,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": this.authToken
+                },
+                data: recordData
+            };
+
+            let res = await axios(config);
+            result = res.data;
+
+            if (result.recordData && result.recordData.records && result.recordData.records.length) {
+                result = result.recordData.records[0];
+            }
+
+            console.log('callContactAPI :: success', result);
+
+        } catch (err: any) {
+
+            console.log('callContactAPI :: error', err);
+
+            let errorData = err.response ? err.response.data : { message: "NO_RESPONSE_RETURN" };
+
+            //try recall if token expire
+            if (errorData.errorCode && errorData.errorCode === 'TOKEN_EXPIRED') {
+                this.authToken = null;
+                result = await this.processContact(pk1, pk2, data);
+            }
+        }
+
+        return result;
+    },
+
+    async register(body: any): Promise<any> {
+        let result = await this.processContact(body.matchColumnName1, body.matchColumnName2, body.data);
+        return result;
+    },
+
+};
